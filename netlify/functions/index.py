a6ac775833b093a72179c0c45f9f915533eb052e
SnailGPT <kartik.ps.mishra07@gmail.com>
2026-02-21 00:36:11 +0530

    Add files via upload

--- netlify/functions/index.py
+++ netlify/functions/index.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Add the root directory to the path so we can import app
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 
 from app import app
 from serverless_wsgi import handle_request
